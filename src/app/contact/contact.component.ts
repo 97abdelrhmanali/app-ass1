78d367f9b0c7b0facda3b1602fa8eeaa31543720
Abdelrhman Ali <abdelrhmanali343@yahoo.com>
2023-03-23 18:05:27 -0700

    upload

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent {
+
+}
+// $(".contact .container .i").keyup(function(){
+//    if($(this).val() != ""){
+//     $(this).prev().removeClass("d-none")
+//    }
+// })
